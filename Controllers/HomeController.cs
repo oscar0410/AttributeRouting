88376d7ad6f0780f5d9651eee5dd87b6b8f447a3
YunYangWu <yunyang.wu@datarget.com>
2022-11-15 11:18:11 +0800

    加入專案檔案。

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using AttributeRouting.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AttributeRouting.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("")]
+        [Route("Home")]
+        [Route("Home/Index")]
+        public string Index()
+        {
+            return "Index() Action Method of HomeController";
+        }
+
+        // Path Variable
+        [Route("Home/Details/{id?}")]
+        public string Details(int id)
+        {
+            return "Details() Action Method of HomeController, ID Value = " + id;
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
